2a206bd1a159754b82338a35020a18bee5344b68
ErickReyes0212 <erickjosepineda33@gmail.com>
2025-02-16 09:05:26 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-      TZ: "America/Tegucigalpa",
-    },
-  };
-  
-  export default nextConfig;
-  
+  env: {
+    TZ: "America/Tegucigalpa",
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "standalone",
+  productionBrowserSourceMaps: false,
+};
+
+export default nextConfig;
